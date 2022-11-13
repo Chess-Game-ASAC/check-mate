4dbd7cedc54bb201479d330b0e6061a9ac5c5ad5
mohannadghbashneh <ghbashneh99@gmail.com>
2022-11-13 20:29:03 +0200

    feat:drawing the board

--- src/chess_main.py
+++ src/chess_main.py
@@ -1,22 +1,24 @@
 import pygame
-import sys
 
-from constant_info import *
+from chess_game import Game 
 
 class Main:
 
   def __init__(self):
     # to initialize pygame library
     pygame.init()
-    self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
-    pygame.display.set_caption('Chess')
+    self.screen = pygame.display.set_mode( (600,600) )
+    pygame.display.set_caption('Check Mate')
+    self.game=Game()
     
   def mainloop(self):
+    
     while True:
+      self.game.show_bg(self.screen)
+      self.game.show_pieces(self.screen)
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
-          sys.exit()
 
       
       pygame.display.update() #should be the last line in the code to update the board
